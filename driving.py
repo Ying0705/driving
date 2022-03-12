6e5ef0dee09708494c04b51c4fba72811aa1c01c
Ying0705 <yy147716@gmail.com>
2022-03-12 13:50:04 +0800

    add US

--- driving.py
+++ driving.py
@@ -1,9 +1,14 @@
 country =  raw_input('please  input your country:')
-age = input('please input your age:')
+age =  raw_input('please input your age:')
 age = int(age)
 if country  ==  'Taiwan' :
 	if age >= 18:
 		print "you can get a driver's license"
 	else:
+		print "you can't get a driver license"
+elif country =='America':
+    if age >= 16 :
+		print "you can get a driver's license"
+    else :
 		print "you can't get a driver license"
 
